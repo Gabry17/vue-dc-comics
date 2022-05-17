d31cce4b76d1471da4d2d7c084f54ae0cd4bd0a2
Gabry17 <gbianchi528@gmail.com>
2022-05-17 16:52:30 +0200

    completato footer top

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,35 @@
 <template>
   <div id="app">
+    <!-- header -->
     <AppHeader />
+
+    <!-- main -->
+    <main class="main"></main>
+
+    <!-- footer -->
+    <AppFooter />
   </div>
 </template>
 
 <script>
 import AppHeader from './components/AppHeader.vue';
+import AppFooter from './components/AppFooter.vue';
 
 
 export default {
   name: 'App',
   components: {
-    AppHeader
+    AppHeader,
+    AppFooter
   }
 }
 </script>
 
 <style lang="scss">
 @import "./css/common.scss";
-
+.main{
+  background-color: aqua;
+  height: 250px;
+}
 
 </style>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,95 @@
+<template>
+    <div class="footer">
+        <div class="foot-top">
+            <div class="link">
+                <div>
+                    <ul>
+                    <li><h3>dc comics</h3></li>
+                    <li><a href="">ciao</a></li>
+                    <li><a href="">ciao</a></li>
+                    <li><a href="">ciao</a></li>
+                    <li><a href="">ciao</a></li>
+                    <li><a href="">ciao</a></li>
+                    <li><a href="">ciao</a></li>
+                </ul>
+                <ul>
+                    <li><h3>shop</h3></li>
+                    <li><a href="">ciao</a></li>
+                    <li><a href="">ciao</a></li>
+                </ul>
+                </div>
+                <ul>
+                    <li><h3>dc</h3></li>
+                    <li><a href="">ciao</a></li>
+                    <li><a href="">ciao</a></li>
+                    <li><a href="">ciao</a></li>
+                    <li><a href="">ciao</a></li>
+                    <li><a href="">ciao</a></li>
+                    <li><a href="">ciao</a></li>
+                </ul>
+                <ul>
+                    <li><h3>sites</h3></li>
+                    <li><a href="">ciao</a></li>
+                    <li><a href="">ciao</a></li>
+                    <li><a href="">ciao</a></li>
+                    <li><a href="">ciao</a></li>
+                    <li><a href="">ciao</a></li>
+                    <li><a href="">ciao</a></li>
+                </ul>
+            </div>
+            <div class="img">
+                <img src="../assets/img/dc-logo-bg.png" alt="">
+            </div>
+
+        </div>
+        <div class="foot-bottom">
+
+        </div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AppFooter'
+}
+</script>
+
+<style lang="scss" scoped>
+$color-dark-color: #212121;
+.footer {
+    background-color: yellow;
+}
+.foot-top {
+    background-image: url("../assets/img/footer-bg.jpg");
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: calc(100vh - 450px);
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    overflow: hidden;
+    li {
+        a{
+            color: #8f8c8c;
+        }
+        h3 {
+            color: white;
+            text-transform: uppercase;
+            margin-bottom: 1em;
+        }
+    }
+}
+
+.link{
+    display: flex;
+    ul{
+        margin:1em 2em;
+    }
+}
+
+.foot-bottom{
+    height: 100px;
+    background-color: $color-dark-color;
+}
+</style>
